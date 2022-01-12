813181295f4ae7d184eac50075158985b086728d
EmilFI <45481038+EmilFI@users.noreply.github.com>
2022-01-12 14:02:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-import pycountry
+from random import randint as ra
 
-len(pycountry.countries)
-print ("Whats your name?")
-x = input()
+print ("Guess a number between 1 and 13")
 
-print ("Hello, " + x)
+for i in range(1):
+    x = ra(1,13)
+
+g1 = int(input())
+
+
+if g1 == x:
+    print("Correct!")
+elif g1 < x:
+    print("Higher!")
+else:
+    print("Lower!")
+    
+g2 = int(input())
+
+if g2 == x:
+    print("Correct!")
+else:
+    print("You loose!")
+
+print("It was " + str(x))
