1ef3d928017a424a584c2f01389a4623f9b82ab0
EmilFI <45481038+EmilFI@users.noreply.github.com>
2022-01-13 13:18:44 +0200

    Comments
    -added comments
    -added if statement to give out correct answer if guess is wrong

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from random import randint as ra
 
+#start of game
 print ("Guess a number between 1 and 13")
 
+#PC picking a card
 for i in range(1):
-    x = ra(1,13)
+    x = ra(1, 13)
 
+#Player guess 1
 g1 = int(input())
 
-
+#Checking if guess 1 is correct
 if g1 == x:
     print("Correct!")
 elif g1 < x:
     print("Higher!")
 else:
     print("Lower!")
-    
+
+#if guess 1 is wrong, guess 2    
 g2 = int(input())
 
+#check if guess 2 is correct
 if g2 == x:
     print("Correct!")
 else:
     print("You loose!")
 
-print("It was " + str(x))
+#print the correct answer if both guesses are wrong
+if g1 and g2 != x:
+    print("The number was " + str(x))
