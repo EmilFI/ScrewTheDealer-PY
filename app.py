be05446b07fa2644640681fc63303f3815fd320f
EmilFI <45481038+EmilFI@users.noreply.github.com>
2022-01-14 10:02:58 +0200

    Check input validity

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
-from random import randint as ra
-
-#start of game
-print ("Guess a number between 1 and 13")
+import random
 
 #PC picking a card
-for i in range(1):
-    x = ra(1, 13)
+x = random.randint(1, 13)
 
-#Player guess 1
-g1 = int(input())
+for i in range(2):
+    g1 = 99
+    #Checking player guess validity
+    while ((type(g1) != int) or g1 > 13 or g1 < 1):
+        try:
+            print ("Guess a number between 1 and 13")
+            g1 = int(input())
+        except ValueError:
+            print("Only numbers!")
 
-#Checking if guess 1 is correct
-if g1 == x:
-    print("Correct!")
-elif g1 < x:
-    print("Higher!")
-else:
-    print("Lower!")
-
-#if guess 1 is wrong, guess 2    
-g2 = int(input())
-
-#check if guess 2 is correct
-if g2 == x:
-    print("Correct!")
-else:
-    print("You loose!")
+    #Checking if guess is correct
+    if g1 == x:
+        print("Correct!")
+        break
+    elif i == 0:
+        if g1 < x:
+            print("Higher!")
+        else:
+            print("Lower!")
 
 #print the correct answer if both guesses are wrong
-if g1 and g2 != x:
+if g1 != x:
     print("The number was " + str(x))
